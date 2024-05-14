dff84bccae48d4028483b1f25b0b228bf96b7207
RubenStap <120170289+RubenStap@users.noreply.github.com>
2024-05-14 17:58:57 +0200

    add(hele opdracht)

---
+++ LogLog/Program.cs
@@ -0,0 +1,25 @@
+﻿using Loggers;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            MyLogger logger = new MyLogger("testclass");
+            MyLogger logger1 = new MyLogger("balta blud");
+
+            logger1.Info("een andere error");
+
+            logger.Debug("Het programma draait op localhost:8000");
+            logger.Info("Het programma is succesvol gestart");
+            logger.Warning("De harde schijf is bijna vol, zorg dat je ruimte maakt");
+            logger.Error("Er is een fout opgetreden in de database, start mysql opnieuw op!");
+            logger.Log("SPACESHIP", "Hallo aarde!");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
---
+++ Loggers/MyLogger.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Loggers
+{
+    public class MyLogger
+    {
+        public string Origin { get; set; }
+
+        public MyLogger(string origin)
+        {
+            if (string.IsNullOrWhiteSpace(origin))
+            {
+                throw new ArgumentException("Origin cannot be null or empty.");
+            }
+            Origin = origin;
+        }
+
+        public void Log(string level, string message)
+        {
+            string print = LogWithTime(level, message);
+            Console.WriteLine(print);
+        }
+
+        public void Warning(string message)
+        {
+            string print = LogWithTime("Warning", message);
+            Console.WriteLine(print);
+        }
+
+        public void Error(string message)
+        {
+            string print = LogWithTime("Error", message);
+            Console.WriteLine(print);
+        }
+
+        public void Info(string message)
+        {
+            string print = LogWithTime("Info", message);
+            Console.WriteLine(print);
+        }
+
+        public void Debug(string message)
+        {
+            string print = LogWithTime("Debug", message);
+            Console.WriteLine(print);
+        }
+
+        private string LogWithTime(string level, string message)
+        {
+            string time = DateTime.Now.ToString("HH:mm:ss tt") + " " + Origin + " " + level + ":" + message;
+            return time;
+        }
+    }
+}
